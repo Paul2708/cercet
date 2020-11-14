7bf4d645e85ff7797cf42721ca889ab41351790e
Paul2708 <paul.hoger@gmx.net>
2020-11-14 19:14:29 +0100

    Initial spring setup

---
+++ backend/src/test/java/de/paul2708/cercet/CercetApplicationTests.java
@@ -0,0 +1,13 @@
+package de.paul2708.cercet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CercetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
