43323d196f15242d2164486d58908f67c1edb272
NyCodeGHG <nycode@protonmail.com>
2020-11-16 20:14:28 +0100

    Added simple code editor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MonacoEditorModule} from 'ngx-monaco-editor';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CodeComponent} from './components/code/code.component';
 import {LoginComponent} from './pages/login/login.component';
 import {StudentComponent} from './pages/student/student.component';
 
@@ -18,7 +18,8 @@ import {StudentComponent} from './pages/student/student.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    StudentComponent
+    StudentComponent,
+    CodeComponent
   ],
   imports: [
     BrowserModule,
