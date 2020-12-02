d2ae727c7c7e46f10646f0e60eae4c1e69a3863f
Paul2708 <paul.hoger@gmx.net>
2020-12-02 16:17:22 +0100

    Print errors

--- backend/server/src/main/java/de/paul2708/server/ws/event/ErrorListener.java
+++ backend/server/src/main/java/de/paul2708/server/ws/event/ErrorListener.java
@@ -1,6 +1,7 @@
 package de.paul2708.server.ws.event;
 
 import io.javalin.websocket.WsContext;
+import io.javalin.websocket.WsErrorContext;
 
 public class ErrorListener implements EventListener {
 
@@ -8,5 +9,14 @@ public class ErrorListener implements EventListener {
     public void handle(WsContext context) throws Exception {
         // Nothing to do atm
         System.out.println("[WS] An error occurred.");
+
+        if (context instanceof WsErrorContext) {
+            WsErrorContext errorContext = (WsErrorContext) context;
+            if (errorContext.error() == null) {
+                return;
+            }
+
+            errorContext.error().printStackTrace();
+        }
     }
 }
