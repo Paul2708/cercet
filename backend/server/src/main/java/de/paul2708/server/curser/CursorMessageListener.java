ae47f13f5e7e2370c81415055450ac399872c28d
Paul2708 <paul.hoger@gmx.net>
2020-12-05 19:56:38 +0100

    Fix exception if user disconnects and the teacher still clicks into the editor

--- backend/server/src/main/java/de/paul2708/server/curser/CursorMessageListener.java
+++ backend/server/src/main/java/de/paul2708/server/curser/CursorMessageListener.java
@@ -7,6 +7,7 @@ import de.paul2708.server.ws.Broadcaster;
 import de.paul2708.server.ws.message.MessageListener;
 import io.javalin.websocket.WsContext;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class CursorMessageListener implements MessageListener {
@@ -24,10 +25,13 @@ public class CursorMessageListener implements MessageListener {
 
     @Override
     public void handle(WsContext ctx, JsonObject data) {
-        User user = userRegistry.findUser(UUID.fromString(data.get("student-uid").getAsString())).get();
+        Optional<User> userOpt = userRegistry.findUser(UUID.fromString(data.get("student-uid").getAsString()));
+        if (userOpt.isEmpty()) {
+            return;
+        }
 
         data.addProperty("teacher-uid", userRegistry.findUser(ctx).get().getUuid().toString());
 
-        user.send(data);
+        userOpt.get().send(data);
     }
 }
