6cd97723208139661f5088ccb012af38cd2f21d3
Paul2708 <paul.hoger@gmx.net>
2020-12-05 19:25:34 +0100

    Cast connect context

--- backend/server/src/main/java/de/paul2708/server/ws/event/ConnectListener.java
+++ backend/server/src/main/java/de/paul2708/server/ws/event/ConnectListener.java
@@ -1,12 +1,14 @@
 package de.paul2708.server.ws.event;
 
+import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsContext;
-import org.w3c.dom.events.Event;
 
 public class ConnectListener implements EventListener {
 
     @Override
     public void handle(WsContext context) throws Exception {
+        WsConnectContext connectContext = (WsConnectContext) context;
+
         // Nothing to do atm.
         // The actual registration is defined in login message listener.
     }
