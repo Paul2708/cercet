c1ab74a65da67f47733e2e0ba14afdddc9bb7b05
Paul2708 <paul.hoger@gmx.net>
2020-12-06 19:41:59 +0100

    Fix typos

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  socketUrl: 'ws://de.paul2708.de/api/ws',
-  serverUrl: 'http://de.paul2708.de/api/'
+  socketUrl: 'ws://cercet.paul2708.de/api/ws',
+  serverUrl: 'http://cercet.paul2708.de/api/'
 
 };
