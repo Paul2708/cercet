9e660b5bb45c649cb648b7f95dfc0cb48f11e13a
NyCodeGHG <nycode@protonmail.com>
2020-12-28 17:10:33 +0100

    Added WebSocket connection and login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,37 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
+
+    const [isAuthenticated, setAuthenticated] = useState(false);
+    const socket = useRef(null);
+
+    useEffect(() => {
+        socket.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+        socket.current.onopen = () => console.log("WebSocket opened!");
+        socket.current.onclose = () => console.log("WebSocket closed!");
+
+        return () => {
+            socket.current.close();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!socket.current) return;
+
+        socket.current.onmessage = event => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+        };
+    });
+
     return (
         <Router>
             <Switch>
                 <Route path="/login">
-                    <Login/>
+                    <Login isAuthenticated={isAuthenticated} setAuthenticated={setAuthenticated}/>
                 </Route>
             </Switch>
         </Router>
--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,21 +1,35 @@
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {Redirect} from "react-router-dom";
 import './Login.css';
+import {useState} from "react";
 
-export default function Login() {
+export default function Login(props) {
+    const [name, setName] = useState("");
+
+    if (props.isAuthenticated) {
+        return <Redirect to="/"/>;
+    }
+
+    function login(event) {
+        event.preventDefault();
+        props.setAuthenticated(true);
+    }
+
     return (
         <Container>
             <Row className="justify-content-center">
-                <Form>
+                <Form onSubmit={login}>
                     <Form.Row className="align-items-center">
                         <Col xs="auto">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username"/>
+                            <Form.Control type="text" placeholder="Enter username" value={name}
+                                          onChange={e => setName(e.target.value)}/>
                             <Form.Text className="text-muted">
                                 Very cool text below the login.
                             </Form.Text>
                         </Col>
                         <Col xs="auto">
-                            <Button type="submit">Login</Button>
+                            <Button type="submit" disabled={name === ''}>Login</Button>
                         </Col>
                     </Form.Row>
                 </Form>
