eea59727e429b9519603924d87a2a4432820b471
NyCodeGHG <nycode@protonmail.com>
2020-12-30 20:43:51 +0100

    Removed debug message

--- frontend/src/components/Logs.js
+++ frontend/src/components/Logs.js
@@ -1,6 +1,7 @@
 export default function Logs(props) {
-    console.log('Hello World');
     return <div>
-        {props.logs.map((log, index) => (<p key={index} className={log.type.toLowerCase()}>{log.output}</p>))}
+        {props.logs.map((log, index) => (
+            <p key={index} className={log.type.toLowerCase()}>{log.output}</p>
+        ))}
     </div>
 }
